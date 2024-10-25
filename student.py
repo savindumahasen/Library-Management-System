a37ea5c6533e551a83b84785a5b96cd4043d2135
Savindu <savinduruhunuhewa@gmail.com>
2024-10-25 14:55:50 +0530

    Update the code and add the comments

--- student.py
+++ student.py
@@ -17,20 +17,13 @@ class Student:
         # Title at the top
         title = Label(self.root, text="Student Management System", font=("times", 30, "bold"), bg="lightblue", fg="black")
         title.pack(side=TOP, fill=X)
-
-        lbl_index= Label(self.root, text="Index Number", font=("times", 14, "bold"),fg='black',bg="lightblue").place(x=10,y=80,width=150)
         
-        lbl_name= Label(self.root, text="Student Name", font=("times", 14, "bold"),fg='black',bg="lightblue").place(x=10,y=120,width=150)
-
+        # Add the labels
+        lbl_index= Label(self.root, text="Index Number",font=("times", 14, "bold"),fg='black',bg="lightblue").place(x=10,y=80,width=150)
+        lbl_name= Label(self.root, text="Student Name",font=("times", 14, "bold"),fg='black',bg="lightblue").place(x=10,y=120,width=150)
         lbl_address= Label(self.root, text="Home Address", font=("times", 14, "bold"),fg='black',bg="lightblue").place(x=10,y=160, width=150)
-
-        
         lbl_contact= Label(self.root, text="Student Telno", font=("times", 14, "bold"),fg='black',bg="lightblue").place(x=10,y=200, width=150)
-
-        
         lbl_email= Label(self.root, text="Student Email", font=("times", 14, "bold"),fg='black',bg="lightblue").place(x=10,y=240, width=150)
-
-        
         lbl_class= Label(self.root, text="Student Class", font=("times", 14, "bold"),fg='black',bg="lightblue").place(x=10,y=280, width=150)
 
 if __name__ == "__main__":
